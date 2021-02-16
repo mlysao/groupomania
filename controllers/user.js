3839eef652f27cb397bb34960f8718111c2f284c
Magali LY SAO <magali.ly-sao@informatique.gov.pf>
2021-02-16 08:17:01 -1000

    email n'est plus crypté

--- controllers/user.js
+++ controllers/user.js
@@ -5,51 +5,24 @@ const maskData = require('maskdata');
 const passwordValidator = require('password-validator');
 
 exports.signup = (req, res, next) => {
-    let userExist = null;
     const schema = new passwordValidator();
-    schema.is().min(8);
-    const validPassword = schema.validate(req.body.password);
-
-    if (validPassword) {
-        User.find()
-            .then(users => {
-                for (let i = 0; i < users.length; i++) {
-                    if (bcrypt.compareSync(req.body.email, users[i].email)) {
-                        userExist = users[i];
-                        break;
-                    }
-                }
-
-                if (userExist) {
-                    return res.status(400).json({error: 'Utilisateur déjà existant !'});
-                }
-
-                const user = new User({
-                    email: bcrypt.hashSync(req.body.email, 10),
-                    emailDisplay: maskData.maskEmail2(req.body.email),
-                    password: bcrypt.hashSync(req.body.password, 10)
-                });
-                user.save()
-                    .then(() => res.status(201).json({message: 'Utilisateur créé !'}))
-                    .catch(error => res.status(400).json({error}));
-            })
+    if (schema.is().min(8).validate(req.body.password)) {
+        const user = new User({
+            email: req.body.email,
+            emailDisplay: maskData.maskEmail2(req.body.email),
+            password: bcrypt.hashSync(req.body.password, 10)
+        });
+        user.save()
+            .then(() => res.status(201).json({message: 'Utilisateur créé !'}))
+            .catch(error => res.status(400).json({error}));
     } else {
         return res.status(400).json({message: 'Le mot de passe doit faire au moins 8 caractères !'});
     }
 };
 
 exports.login = (req, res, next) => {
-    let user = null;
-    User.find()
-        .then(users => {
-            for (let i = 0 ; i < users.length ; i++) {
-                if (bcrypt.compareSync(req.body.email, users[i].email)) {
-                    console.log(users[i]);
-                    user = users[i];
-                    break;
-                }
-            }
-
+    User.findOne({email: req.body.email})
+        .then(user => {
             if (!user) {
                 return res.status(401).json({error: 'Utilisateur non trouvé !'});
             }
